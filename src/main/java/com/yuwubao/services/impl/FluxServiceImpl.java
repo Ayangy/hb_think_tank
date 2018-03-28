dabde87010d6428aa494022e8045fb75369aceac
aipper <aipper@qq.com>
2018-03-28 15:55:39 +0800

    bug

--- src/main/java/com/yuwubao/services/impl/FluxServiceImpl.java
+++ src/main/java/com/yuwubao/services/impl/FluxServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 
 @Service
-public class FluxServiceImpl  implements FluxService{
+public class FluxServiceImpl implements FluxService {
     @Resource
     private FluxRepo fluxRepo;
 
@@ -17,7 +17,7 @@ public class FluxServiceImpl  implements FluxService{
     public FluxEntity getFlux() {
         FluxEntity entity = fluxRepo.findOne(1);
         int num = entity.getFluxnum();
-        entity.setFluxnum(num++);
+        entity.setFluxnum(num + 1);
         fluxRepo.save(entity);
         return entity;
     }
